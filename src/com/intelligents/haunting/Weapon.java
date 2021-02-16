f658c62f5c5d5ef20269a8c6a24fd9b1ca7edd89
Collin Fairlie <collin.fairlie@gmail.com>
2021-02-16 11:35:05 -0800

    #AB535 create a weapon class with name, description and damage points.

---
+++ src/com/intelligents/haunting/Weapon.java
@@ -0,0 +1,15 @@
+package com.intelligents.haunting;
+
+public class Weapon {
+
+    private String name;
+    private String description;
+    private int damagePoints;
+
+    Weapon(String name, String description, int damagePoints) {
+        this.name = name;
+        this.description = description;
+        this.damagePoints = damagePoints;
+    }
+
+}
